6ec9b29879db86b27eed5b34dea6409b1ec89a8b
CensuradoBrazil <paulinacarvalhocosta@gmail.com>
2025-04-29 20:36:04 -0300

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -376,9 +376,9 @@ default_vae = get_config_item_or_set_default(
 default_styles = get_config_item_or_set_default(
     key='default_styles',
     default_value=[
-        "Fooocus V2",
-        "Fooocus Enhance",
-        "Fooocus Sharp"
+        "KevinIA V2",
+        "KevinIA Enhance",
+        "KevinIA Sharp"
     ],
     validator=lambda x: isinstance(x, list) and all(y in modules.sdxl_styles.legal_style_names for y in x),
     expected_type=list
